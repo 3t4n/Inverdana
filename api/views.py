a20b0837d13d88903de61adcf03aa9db461ad9b1
Etan <A01209675@itesm.mx>
2019-09-29 16:26:33 -0500

    Added alexis views

--- api/views.py
+++ api/views.py
@@ -5,10 +5,22 @@ from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from django.contrib.auth.decorators import login_required
 
 
 
 User = get_user_model()
+
+def login(request):
+        return render(request, 'login.html')
+
+def logintest():
+    return '<h1>prueba</h1>'
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
 
 class UserViewSet(views.UserViewSet):
      def retrieve(self, request, *args, **kwargs):
