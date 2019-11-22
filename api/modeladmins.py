9173e538cdba2bf0da281d7489089291b8ffbb36
Etan <A01209675@itesm.mx>
2019-11-22 06:36:58 -0600

    Custom filters and search bar for trees

--- api/modeladmins.py
+++ api/modeladmins.py
@@ -4,4 +4,23 @@ from .models import *
 class QrCodeAdmin(ModelAdmin):
     model = Identifier.QRcode
     exclude = ['string']
-    add_form_template='admin/qrcodes/add.html'
+    list_display = ('string', 'status')
+    list_filter = ('status',)
+#    add_form_template='admin/qrcodes/add.html'
+
+class TreeAdmin(ModelAdmin):
+    model = Tree.Tree
+    list_display=('nombre','edad','especie')
+    list_filter = ('age','specie_id')
+    search_fields =('name',)
+    def edad(self,obj):
+        return obj.age
+    edad.short_description = "Edad"
+    def nombre(self,obj):
+        return obj.name
+    nombre.short_description = "Nombre"
+    def especie(self,obj):
+        return obj.specie_id
+    especie.short_description = "Especie"
+        
+
--- api/models/Tree.py
+++ api/models/Tree.py
@@ -5,6 +5,7 @@ from django.contrib.gis.db import models as geomodels
 class TreeSpecie(models.Model):
     commonname = models.CharField(max_length=100, blank=False)
     sciname = models.CharField(max_length=100, blank=False)
+    
     class Meta:
         verbose_name_plural = "Especies de árboles"
         verbose_name="especie de árbol"
@@ -14,18 +15,18 @@ class TreeSpecie(models.Model):
 class TreeState(models.Model):
     name = models.CharField(max_length=100, blank=False)
     class Meta:
-        verbose_name_plural = "Estados"
+        verbose_name_plural = "Estados catálogo"
         verbose_name="estado"
     def __str__(self):
         return '%s' % (self.name)
 
 
 class Tree(models.Model):
-    specie_id = models.ForeignKey(TreeSpecie, on_delete=models.SET_NULL, null=True)
+    specie_id = models.ForeignKey(TreeSpecie, on_delete=models.SET_NULL, null=True, verbose_name="Especie")
     shareholders = models.ManyToManyField(User, through='Share')
     states = models.ManyToManyField(TreeState, through='HasState')
     name = models.CharField(max_length=100, blank=False)
-    age = models.IntegerField(default=0)
+    age = models.IntegerField(default=0, verbose_name="Edad")
     point = geomodels.PointField()
     class Meta:
         verbose_name_plural = "Árboles"
